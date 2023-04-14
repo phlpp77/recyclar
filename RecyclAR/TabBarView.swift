18e8625b5831c8aa10bb741f41ff2d29a67421d0
Park, Soo Bin <sbpark422@naver.com>
2023-04-13 22:40:25 -0400

    Update TabBarView.swift

--- RecyclAR/TabBarView.swift
+++ RecyclAR/TabBarView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TabBarView: View {
     
-    @State private var selectedTab = 0
+    @State private var selectedTab = 1
     
     var body: some View {
         NavigationStack {
