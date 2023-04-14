d474cc7c41bba63253b1eaf0a0997b2d03945231
phlpp77 <PhilippHemkemeyer@iCloud.com>
2023-04-14 17:53:38 -0400

    Add price to product model

--- RecyclAR/ProductModel.swift
+++ RecyclAR/ProductModel.swift
@@ -10,15 +10,16 @@ import Foundation
 struct Product: Identifiable {
     
     static var all: [Product] = [
-        Product(name: "Cheerios", imageName: "cheerios", sustainabilityPoints: 80),
-        Product(name: "Cheerios", imageName: "cheerios", sustainabilityPoints: 80),
-        Product(name: "Cheerios", imageName: "cheerios", sustainabilityPoints: 80),
-        Product(name: "Cheerios", imageName: "cheerios", sustainabilityPoints: 80),
-        Product(name: "Pure Life Water", imageName: "pureLifeWater", sustainabilityPoints: 60)
+        Product(name: "Cheerios", imageName: "cheerios", sustainabilityPoints: 80, price: 1.0),
+        Product(name: "Cheerios", imageName: "cheerios", sustainabilityPoints: 80, price: 1.0),
+        Product(name: "Cheerios", imageName: "cheerios", sustainabilityPoints: 80, price: 1.0),
+        Product(name: "Cheerios", imageName: "cheerios", sustainabilityPoints: 80, price: 1.0),
+        Product(name: "Pure Life Water", imageName: "pureLifeWater", sustainabilityPoints: 60, price: 1.49)
     ]
     
     var name: String
     var imageName: String
     var sustainabilityPoints: Int
+    var price: Double
     var id = UUID()
 }
